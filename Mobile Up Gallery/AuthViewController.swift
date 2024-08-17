1fed9da8e39b05453f355cb696ae54a7169ba640
ivanych2202 <ivanych2202@yandex.ru>
2024-08-17 15:05:00 +0300

    Настройка WebView для авторизации, создание модального представления для WebView, обработка успешной и неуспешной авторизации.

--- Mobile Up Gallery/AuthViewController.swift
+++ Mobile Up Gallery/AuthViewController.swift
@@ -6,20 +6,73 @@
 //
 
 import UIKit
+import WebKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController{
 
     @IBOutlet weak var loginButton: UIButton!
     
+    @IBOutlet weak var webViewAuth: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webViewAuth.navigationDelegate = self
+        
+        webViewAuth.isHidden = true
         
         
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "AuthToMain", sender: sender)
+        
+//        let url = URLComponents()
+        
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = "oauth.vk.com"
+        urlComponent.path = "/authorize"
+        
+        urlComponent.queryItems = [
+            URLQueryItem(name: "client_id", value: "52163510"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "responce_type", value: "token")]
+        
+        if let url = urlComponent.url {
+            let request = URLRequest(url: url)
+            webViewAuth.load(request)
+        } else {
+            showAlert()
+        }
+        webViewAuth.isHidden = false
     }
     
-
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Ошибка", message: "Не удалось авторизоваться в ВКонтакте. Попробуйте еще раз.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
+
+
+extension AuthViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        guard let url = webView.url else { return }
+        
+        if url.host == "oauth.vk.com" && url.path == "/blank.html" {
+            if let fragment = url.fragment {
+                if fragment.hasPrefix("code=") {
+                    webViewAuth.isHidden = true
+                    self.performSegue(withIdentifier: "AuthToMain", sender: self)
+                } else {
+                    showAlert()
+                }
+            }
+        }
+    }
+}
+
